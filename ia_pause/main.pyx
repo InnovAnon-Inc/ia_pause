6efeeddf8631f5b3030a613eb8e720c087d4f02a
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2024-11-14 12:52:36 -0600

    Initial Commit

--- ia_pause/main.pyx
+++ ia_pause/main.pyx
@@ -21,25 +21,36 @@ async def tick(bar:Bar, interval:float,)->None:
 	bar.next()
 	await asyncio.sleep(interval,)
 
-async def _main(message:str='Momentarily Paused', max_value:int=20, wait_time:float=5,)->None:
+async def ticker(message:str, max_value:int, interval:float,)->None:
+	with Bar(message, max=max_value,) as bar:
+		for i in range(max_value):
+			await tick(bar=bar, interval=interval,)		
+
+async def _main(message:str='Momentarily Paused', max_value:int=20, wait_time:float=5,)->bool:
 	assert (max_value > 0)
 	assert (wait_time > 0)
 	interval:float = wait_time / float(max_value)
 	assert (interval  > 0)
-	with Bar(message, max=max_value,) as bar:
-		for i in range(max_value):
-			await tick(bar=bar, interval=interval,)		
+	try:
+		await ticker(message:str, max_value:int, interval:float,)
+		return True
+	except KeyboardInterrupt as error:
+		await logger.aerror(error)
+		return False
 
 def main()->None:
 	message  :str   =     os.getenv('PAUSE_MESSAGE', 'Waiting for you to come to your senses')
 	max_value:int   = int(os.getenv('PAUSE_COUNT',   '20'))
 	wait_time:float = int(os.getenv('PAUSE_TIMER',    '5'))
-	asyncio.run(_main(
+	result   :bool  = asyncio.run(_main(
 		message=message,
 		max_value=max_value,
 		wait_time=wait_time,
 	))
-	logger.info('Proceeding')
+	if result:
+		logger.info('Proceeding')
+	else:
+		logger.info('Terminated')
 
 if __name__ == '__main__':
 	main()
